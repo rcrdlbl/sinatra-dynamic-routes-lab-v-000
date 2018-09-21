1d8a3e5bdb820fa002711bb288faf57ec27a62ea
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-21 20:09:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    :number.to_i.times do
-      "#{:phrase}"
+    params[:number].to_i.times do
+      "#{params[:phrase]}"
     end
   end
 
